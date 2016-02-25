8c4184826b39edb20ee72f2d05b6a87268169f9f
Jodie Alaine Parker <me@jodiealaine.com>
2016-02-25 01:45:47 +0000

    Add ability for the create method to return the object created

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -6,10 +6,11 @@ class Udacidata
 	@@num = 0
   # Your code goes here!
   def self.create(attributes=nil)
-    id = (self.new attributes).id
+    item = self.new attributes
     data_path = File.dirname(__FILE__) + "/../data/data.csv"
-    CSV.open(data_path, "a+") do |csv|
-     	csv << [id, attributes[:brand], attributes[:name], attributes[:price]]
+    CSV.open(data_path, "a") do |csv|
+     	csv << [item.id, attributes[:brand], attributes[:name], attributes[:price]]
     end
+    item
   end
 end
