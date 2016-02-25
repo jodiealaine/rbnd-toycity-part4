f435083bb3205770b1372f406324bdf1acd3b271
Jodie Alaine Parker <me@jodiealaine.com>
2016-02-25 01:32:22 +0000

    Add create method to udacidata

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,13 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
+	@@num = 0
   # Your code goes here!
+  def self.create(attributes=nil)
+    id = (self.new attributes).id
+    data_path = File.dirname(__FILE__) + "/../data/data.csv"
+    CSV.open(data_path, "a+") do |csv|
+     	csv << [id, attributes[:brand], attributes[:name], attributes[:price]]
+    end
+  end
 end
