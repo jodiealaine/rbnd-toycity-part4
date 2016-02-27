b2a71d820f029e1cde1b5b2ab7841f120afd6fd6
Jodie Alaine Parker <me@jodiealaine.com>
2016-02-27 15:56:23 +0000

    Refactor all method to open the CSV to get data

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -3,7 +3,7 @@ class Module
     attributes.each do |attribute|
         new_finder_method = %{
           def self.find_by_#{attribute}(item)	
-          	@@all.each {|product| return product if product.#{attribute} == item}	
+          	self.all.each {|product| return product if product.#{attribute} == item}	
           end
     	}
 		self.class_eval new_finder_method
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,56 +4,61 @@ require 'csv'
 
 class Udacidata  
 	create_finder_methods :brand, :name
-	@@all = []
   def self.create(attributes=nil)
     item = self.new attributes
-    CSV.open(self.data_path, "a+") do |csv|
-     	csv << [item.id, item.brand, item.name, item.price]
+    # p item.brand
+    # p item.name 
+    # p item.price
+    unless self.item_exists? item
+			CSV.open(self.data_path, "a+") do |csv|
+     		csv << [item.id, item.brand, item.name, item.price]  
+    	end
     end
-   	@@all << item
    	item
   end
 
   def self.all
-  	@@all
+  	product_instances = []
+  	all = CSV.table(self.data_path, write_headers: true)
+  	all.each do|row|
+  	  product_instances << self.create(id: row[:id], brand: row[:brand], name: row[:product], price: row[:price])
+  	end
+  	product_instances
   end
 
   def self.first num=1
-  	return @@all.first if num == 1 
-		first_num_rows = CSV.table(self.data_path, write_headers: true).first(num)
-  	first_num = []
-  	first_num_rows.each do |row|
-  	 	first_num << self.find(row[:id])
-  	end
-  	first_num
+  	return self.all.first if num == 1 
+		self.all.first(num)
   end
 
   def self.last num=1
-  	num == 1 ? @@all.last : @@all.last(num)
+  	num == 1 ? self.all.last : self.all.last(num)
   end
 
   def self.find id
-  	@@all.each {|product| return product if product.id == id }
+  	self.all.each {|product| return product if product.id == id }
   	raise ProductNotFoundError, "Product id:#{id} not found"
   end
 
   def self.destroy id
+  	
+    deleted_product = self.find id
   	raise ProductNotFoundError, "Product id:#{id} not found" if self.find(id) == false 
   	
   	data_table = CSV.table(self.data_path, write_headers: true)
-		data_table.delete_if do |row|
-  		row[:id] == id
-		end
 		
-		File.open(self.data_path, 'w') do |f|
-  		f.write(data_table.to_csv)
-		end
-		@@all.delete(self.find id)
+		updated_data_table = data_table.delete_if {|row| row[:id] == id}
+
+		File.open(self.data_path, "w") do |f|
+ 			f.write(updated_data_table.to_csv)
+  	end
+
+  	deleted_product
   end
   
   def self.where options={}
 		products = []
-  	@@all.each do |product|
+  	self.all.each do |product|
   	  products << product if (product.brand == options[:brand] || product.name == options[:name])
   	end
   	products
@@ -74,6 +79,13 @@ class Udacidata
   		f.write(data_table.to_csv)
 		end
 		self
+  end
+
+  def self.item_exists? item
+  	database = CSV.read(self.data_path)
+    item_exists = false
+    database.each {|row| item_exists = true if row[0] == item.id.to_s }
+    item_exists
   end
 
   def self.data_path
